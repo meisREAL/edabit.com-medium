1e1307b406a42cf195de49d335f593908a6da24a
meisReal <simas.cesnakauskas@gmail.com>
2023-01-13 14:48:30 +0200

    6

--- medium.js
+++ medium.js
@@ -102,3 +102,16 @@ function num_of_digits(num) {
     const count = (String(num).match(/[0-9]/g) || []).length
     return count
 }
+
+// Given three arguments ⁠— an object obj of the stolen items, the pets name and a value ⁠— return an object with that name and value in it (as key-value pairs).
+// Examples
+// addName({}, "Brutus", 300) ➞ { Brutus: 300 }
+// addName({ piano: 500 }, "Brutus", 400) ➞ { piano: 500, Brutus: 400 }
+// addName({ piano: 500, stereo: 300 }, "Caligula", 440) ➞ { piano: 500, stereo: 300, Caligula: 440 }
+// Notes
+// The value argument will be a number.
+
+function addName(obj, name, value) {
+    obj[name] = value;
+    return obj;
+}
