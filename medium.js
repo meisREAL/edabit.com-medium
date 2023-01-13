a71f52a9f3783323c9fcc3c2d8c235eee80d171e
meisReal <simas.cesnakauskas@gmail.com>
2023-01-13 18:11:31 +0200

    8

--- medium.js
+++ medium.js
@@ -127,3 +127,14 @@ function addName(obj, name, value) {
 function derivative(b, m) {
     return b * m ** (b - 1);
 }
+
+
+//* A tetrahedron is a pyramid with a triangular base and three sides.A tetrahedral number is a number of items within a tetrahedron.
+// Create a function that takes an integer n and returns the nth tetrahedral number.
+// Examples
+// tetra(2) ➞ 4
+// tetra(5) ➞ 35
+// tetra(6) ➞ 56
+function tetra(n) {
+    return (n * (n + 1) * (n + 2)) / 6
+}
