bd54a9fd5b08041e747ba89918dad96cc82a7ec9
meisReal <simas.cesnakauskas@gmail.com>
2023-01-13 12:15:21 +0200

    4

--- medium.js
+++ medium.js
@@ -68,3 +68,21 @@ function possibleBonus(a, b) {
         return false;
     }
 }
+
+// Perimeters with a Catch
+//* Write a function that takes a number and returns the perimeter of either a circle or a square.The input will be in the form(letter l, number num) where the letter will be either "s" for square, or "c" for circle, and the number will be the side of the square or the radius of the circle.
+// Use the following formulas:
+// Perimeter of a square: 4 * side.
+// Perimeter of a circle: 6.28 * radius.
+// The catch is you can only use arithmetic or comparison operators, which means:
+// No if... else statements.
+// No objects.
+// No arrays.
+// No formatting methods, etc.
+// The goal is to write a short, branch - free piece of code.
+
+function perimeter(l, num) {
+    return (4 * num) * (l === 's')
+        + (6.28 * num) * (l === 'c')
+};
+//! jeigu l === s tai skaicius skliaustuose tampa 1, nes true reisksmes yra = 1, jeigu lygybe yra false tai skaicius skliaustuose tampa 0, o daugyba is 0 bus lygi 0. Tada skaicius + 0 = skaiciui.
