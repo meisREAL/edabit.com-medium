8eb5974d38a282b02659dd812f73e29155983723
meisReal <simas.cesnakauskas@gmail.com>
2023-01-15 12:06:37 +0200

    10

--- medium.js
+++ medium.js
@@ -153,3 +153,18 @@ function toArray(obj) {
     // return arr;
     return Object.entries(obj)
 }
+
+//* Create a function that concatenates n input arrays, where n is variable.
+// Examples
+// concat([1, 2, 3], [4, 5], [6, 7]) ➞ [1, 2, 3, 4, 5, 6, 7]
+// concat([1], [2], [3], [4], [5], [6], [7]) ➞ [1, 2, 3, 4, 5, 6, 7]
+// concat([1, 2], [3, 4]) ➞ [1, 2, 3, 4]
+// concat([4, 4, 4, 4, 4]) ➞ [4, 4, 4, 4, 4]
+// Notes
+// Arrays should be concatenated in order of the arguments.
+
+function concat(...args) {
+    let arr = []
+    arr = arr.concat(...args);
+    return arr;
+}
