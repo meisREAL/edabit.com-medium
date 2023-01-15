37922b283786f714323c7af4fcb087146c848336
meisReal <simas.cesnakauskas@gmail.com>
2023-01-15 11:28:39 +0200

    9

--- medium.js
+++ medium.js
@@ -138,3 +138,18 @@ function derivative(b, m) {
 function tetra(n) {
     return (n * (n + 1) * (n + 2)) / 6
 }
+
+//* Write a function that converts an object into an array, where each element represents a key-value pair in the form of an array.
+// Examples
+// toArray({ a: 1, b: 2 }) ➞ [["a", 1], ["b", 2]]
+// toArray({ shrimp: 15, tots: 12 }) ➞ [["shrimp", 15], ["tots", 12]]
+// toArray({}) ➞ []
+// Notes
+// Return an empty array if the object is empty.
+
+function toArray(obj) {
+    // const arr = []
+    // arr.push(Object.entries(obj))
+    // return arr;
+    return Object.entries(obj)
+}
