f0f2635fc30787079966dd09dcb6fbd29db8b396
meisReal <simas.cesnakauskas@gmail.com>
2023-01-15 12:30:20 +0200

    13

--- medium.js
+++ medium.js
@@ -201,3 +201,20 @@ function keysAndValues(obj) {
     return arr;
 }
 
+//* Create a function that takes two numbers as arguments (num, length) and returns an array of multiples of num until the array length reaches length.
+// Examples
+// arrayOfMultiples(7, 5) ➞ [7, 14, 21, 28, 35]
+// arrayOfMultiples(12, 10) ➞ [12, 24, 36, 48, 60, 72, 84, 96, 108, 120]
+// arrayOfMultiples(17, 6) ➞ [17, 34, 51, 68, 85, 102]
+// Notes
+// Notice that num is also included in the returned array.
+
+function arrayOfMultiples(num, length) {
+    let arr = [];
+    let x = 0;
+    for (let i = 0; i < length; i++) {
+        x = x + num;
+        arr.push(x);
+    }
+    return arr;
+}
